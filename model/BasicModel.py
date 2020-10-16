8f03af8aac786c8c1730c84bb7f97e56d62b5b51
karencfisher <karen-fisher@lambdastudents.com>
2020-10-16 11:16:51 -0600

    minor changes

--- model/BasicModel.py
+++ model/BasicModel.py
@@ -8,24 +8,54 @@ from sklearn.neighbors import NearestNeighbors
 
 
 class BasicModel:
+    '''
+    This is a 'wrapper' for the actual model. It allows for 
+    the model to be instantiated, trained, saved, and reloaded.
+    '''
     def __init__(self):
         self.model = None
 
     def build(self, n_suggestions):
+        '''
+        Build the model.
+        Params:
+            n_suggestions - the number of similar items to be
+            returned.
+        '''
         self.model = Model(n_suggestions)
 
     def save(self, file_path):
+        '''
+        Save the trained model
+        Params:
+            file_Path - the path to which file is to be written
+        '''
         assert self.model is not None, 'model not built'
         pickle.dump(self.model, file_path)
 
     def load(self, file_path):
+        '''
+        Re-load the trained model (instead of building)
+        Params:
+            file_Path - the path from which to load
+        '''
         self.model = pickle.load(file_path)
 
     def fit(self, X):
+        '''
+        Fit the model
+        Params:
+            X -- training data. Expected to be a pandas dataframe.
+        '''
         assert self.model is not None, 'model not built'
         return self.model.fit(X)
 
     def predict(self, x):
+        '''
+        Predict similar to example
+        Params:
+            x -- training data. Expected to be a 2-dim array.
+        '''
         assert self.model is not None, 'model not built'
         return self.model.predict(x)
 
@@ -41,8 +71,7 @@ class Model:
         return m
 
     def predict(self, x):
-        x_process = x.reshape(1, -1)
-        x_process = self.scaler.transform(x_process)
+        x_process = self.scaler.transform(x)
         scores, results = self.model.kneighbors(x_process)
         
         return scores, results
